18df82bd30723076f29371174bb5f00c98627ba3
dimitricas7 <dmitriymedvedok95@mail.ru>
2023-12-17 02:29:14 +0300

    Добавлена отзывчивость макета

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    screens: {
+      'tablet': '681px',
+      // => @media (min-width: 681px) { ... }
+      'phone': '579px',
+      'lg': '1440px'
+    },
     extend: {},
   },
   plugins: [],
