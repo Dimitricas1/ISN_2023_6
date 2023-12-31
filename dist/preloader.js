6b88088409920fbc9a0cdd275d18a5e69538fcf0
Dimitricas <dmitriymedvedok95@mail.ru>
2023-12-31 14:49:18 +0300

    Спрятаны полосы прокрутки во время работы прелодера

--- dist/preloader.js
+++ dist/preloader.js
@@ -1,6 +1,7 @@
 window.onload = function(){
 	setTimeout(function(){
         document.getElementById('preloader').classList.add('loaded_hiding');
+        document.body.classList.remove('loading_no_overflow');
 	}, 543
 	);
     setTimeout(function(){
